83f97960900a7b900d7d108bf4bbffc711dd7ecf
kewalg <kgulve1@binghamton.edu>
2019-11-20 14:13:51 -0500

    working on the coordinates

--- app/src/main/java/com/example/myapp/CurrentActivity.java
+++ app/src/main/java/com/example/myapp/CurrentActivity.java
@@ -32,24 +32,31 @@ public class CurrentActivity extends AppCompatActivity {
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
-
             }
 
             @Override
             public void onProviderEnabled(String provider) {
-
             }
 
             @Override
             public void onProviderDisabled(String provider) {
-
             }
         };
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1);
         } else {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
             Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastKnownLocation != null) {
+                updateLocationinfo(lastKnownLocation);
+            }
         }
     }
+
+    public void updateLocationinfo(Location location) {
+        Log.i("Location", location.toString());
+
+    }
+
 }
