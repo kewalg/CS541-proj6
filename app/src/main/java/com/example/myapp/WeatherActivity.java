2714d32f63ae5d4db317036c2b232fd1e662bbfa
kewalg <kgulve1@binghamton.edu>
2019-11-23 18:53:23 -0500

    custom location weather

--- app/src/main/java/com/example/myapp/WeatherActivity.java
+++ app/src/main/java/com/example/myapp/WeatherActivity.java
@@ -2,14 +2,13 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
 
-import com.androdocs.httprequest.HttpRequest;
-
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -17,17 +16,27 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    EditText edt_weather;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        edt_weather = findViewById(R.id.edt_cityname);
+    }
+
+    public void getWeather(View view) {
+        DownloadTask task = new DownloadTask();
+        task.execute("https://openweathermap.org/data/2.5/weather?q=" + edt_weather.getText().toString() + "&appid=b6907d289e10d714a6e88b30761fae22");
+    }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... urls) {
-
             String result = "";
             URL url;
             HttpURLConnection urlConnection = null;
@@ -38,9 +47,10 @@ public class WeatherActivity extends AppCompatActivity {
                 InputStream in = urlConnection.getInputStream();
                 InputStreamReader reader = new InputStreamReader(in);
                 int data = reader.read();
+
                 while (data != -1) {
                     char current = (char) data;
-                    result += current;
+                    result = result + current;
                     data = reader.read();
                 }
                 return result;
@@ -53,18 +63,22 @@ public class WeatherActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.i("JSON", s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String weatherinfo = jsonObject.getString("weather");
+                Log.i("Weather content", weatherinfo);
+                JSONArray arr = new JSONArray(weatherinfo);
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
-
-        DownloadTask task = new DownloadTask();
-        task.execute("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
-    }
 }
-
